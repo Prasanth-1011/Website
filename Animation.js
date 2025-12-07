637e2296c0cd6db04896fcb54372d743b0e8988d
Prasanth-1011 <112630396+Prasanth-1011@users.noreply.github.com>
2025-12-08 01:49:48 +0530

    Update Animation.js

--- Animation.js
+++ Animation.js
@@ -10,3 +10,19 @@ const glitch = () => {
 
 glitch();
 
+// Mobile Menu
+const smallMenu = document.querySelector(".header__mobile__links");
+const iconsMenu = () => {
+
+    if (window.innerWidth <= 576) {
+        smallMenu.innerHTML = `
+            <span class="material-icons menu">menu</span>
+            <a href="#home" class="header__links__home"><i class="material-icons">home</i></a>
+            <a href="#trending" class="header__links__trending"><i class="material-icons">trending_up</i></a>
+            <a href="./Collections.html"><i class="material-icons">local_mall</i></a>
+            <a href="./Contact.html"><i class="material-icons">chat</i></a>
+        `
+    }
+};
+
+iconsMenu();
